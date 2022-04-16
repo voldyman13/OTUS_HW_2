9e54034faa1757217a1b0de2bf270ed9d08bfa32
Vladimir Lapin <51967175+voldyman13@users.noreply.github.com>
2022-04-16 21:51:38 +0300

    fixed iteration over book list

--- src/homework_3.py
+++ src/homework_3.py
@@ -1,29 +1,36 @@
+import csv
 import json
 from csv import DictReader
 
 """ Работа с тестовыми данными """
 
+# Получаем список книг из файла books.csv
 with open('../files/books.csv', newline='') as csv_file:
-    books = DictReader(csv_file)
-    size = books.__sizeof__() - 1
-    with open("../files/users.json", "r") as json_file:
-        users = json.loads(json_file.read())
-        while size > 0:
-            for user in users:
-                if size == 0:
-                    break
-                if "books" not in user:
-                    user["books"] = []
-                # получаем следующую книгу
-                book = next(books)
-                # выдаем книгу
-                user["books"].append(
-                    {"Title": book["Title"], "Author": book["Author"], "Genre": book["Genre"], "Pages": book["Pages"]})
-                size -= 1
-    data = []
-    for user in users:
-        data.append({"name": user["name"], "gender": user["gender"], "address": user["address"], "age": user["age"],
-                     "books": user["books"]})
+    books = list(DictReader(csv_file))
+
+# Получаем список пользователей из файла users.json
+with open("../files/users.json", "r") as json_file:
+    users = json.loads(json_file.read())
+
+index = 0
+for book in books:
+    if index == len(users):
+        index = 0
+    if "books" not in users[index]:
+        user = {
+            "name": users[index]["name"],
+            "gender": users[index]["gender"],
+            "address": users[index]["address"],
+            "age": users[index]["age"],
+            "books": []
+        }
+        # перезаписываем данные пользователя оставляя только необходимые данные и добавляем новый аргумент books[]
+        users[index].clear()
+        users[index] = user
+    # выдаем книгу
+    users[index]["books"].append(
+        {"Title": book["Title"], "Author": book["Author"], "Genre": book["Genre"], "Pages": book["Pages"]})
+    index += 1
 # сохраняем результат в файл в формате json
 with open("../files/result.json", "w") as result_file:
-    json.dump(data, result_file, indent=4)
+    json.dump(users, result_file, indent=4)
